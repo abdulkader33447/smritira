a973c467f2a14fff036a463086ae02ad906c0449
abdulkader33447 <abdulkader33447@gmail.com>
2025-11-25 10:23:26 +0600

    added marquee

---
+++ src/floating/UnderConstaction.jsx
@@ -0,0 +1,14 @@
+import Marquee from "react-fast-marquee";
+
+
+const UnderConstaction = () => {
+  return (
+    <div>
+      <Marquee>
+        <h3 className="text-red-600 uppercase font-semibold italic">This website is under constraction</h3>
+      </Marquee>
+    </div>
+  );
+};
+
+export default UnderConstaction;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,11 +2,13 @@ import Navbar from "@/components/shared/Navbar";
 import Hero from "./sections/Hero/Hero";
 import Cta from "./sections/cta/Cta";
 import Feedback from "./sections/feedback/Feedback";
+import UnderConstaction from "@/floating/UnderConstaction";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <UnderConstaction />
       <Hero />
       {/* Preserving Memories, Celebrating Lives */}
       <Cta />
